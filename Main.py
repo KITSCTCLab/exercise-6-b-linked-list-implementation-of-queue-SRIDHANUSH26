4e29ddc58965097c357352741594832d11986feb
SRIDHANUSH RAJAMANICKAM <109951909+SRIDHANUSH26@users.noreply.github.com>
2022-09-12 14:21:28 +0530

    initial commit 2

--- Main.py
+++ Main.py
@@ -4,48 +4,43 @@ class Node:
     self.next = None
 
 
-class Stack:
+class Queue:
   def __init__(self):
     self.head = None
+    self.last = None
 
-  def push(self, data) -> None:
-    # Write your code here
-    new = Node(data)
-    if self.head == None:
-      new.next = None
-      self.head = new
-    else:
-      new.next = self.head
-      self.head = new
+  def enqueue(self, data) -> None:
+        new = Node(data)
+        if self.last is not None:
+            self.last.next = new
+        if self.head is None:
+            self.head = new
+        self.last = new
 
-  def pop(self) -> None:
-    # Write your code here
-    if self.head == None:
-      pass
-    else:
-      self.head = self.head.next
+  def dequeue(self) -> None:
+        if not self.head is None:
+            self.head = self.head.next
+            if self.head is None:
+              self.last = None
 
-  def status(self):
-    """
-    It prints all the elements of stack.
-    """
-    # Write your code here
-    trav = self.head
-    while trav != None:
-      print(trav.data, "=>",sep = "", end = "")
-      trav = trav.next
-    print("None")
-    
+  def status(self) -> None:
+        current = self.head
+        while current is not None:
+          print(current.data, end = "=>")
+          current = current.next
+        print("None")
+
+
 # Do not change the following code
-stack = Stack()
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-stack.status()
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
